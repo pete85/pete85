e497da5b08598b9ceac662a490465ab6af9d44f3
pete85 <info.pete85@gmail.com>
2017-04-28 16:43:19 +0100

    toggle-javascript initial commit

---
+++ toggle-javascript/include/head.php
@@ -0,0 +1,14 @@
+<meta charset="utf-8">
+<meta http-equiv="X-UA-Compatible" content="IE=edge">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<meta name="description" content="ESTABLISHED IN 1998, STREAM IS AN INDEPENDENT, PRIVATELY OWNED, FULL SERVICE CREATIVE AGENCY THAT’S PASSIONATE ABOUT BUILDING BRANDS AND SALES. EXCELLENCE IS AT THE HEART OF WHAT WE DO">
+<meta name="author" content="Stream Ltd">
+
+<title>Stream Ltd | Intelligent thinking Inspired ideas </title>
+
+<!-- Bootstrap Core CSS -->
+<link href="css/bootstrap.min.css" rel="stylesheet">
+
+
+<!-- Custom CSS -->
+<link href="css/styles.css" rel="stylesheet">
