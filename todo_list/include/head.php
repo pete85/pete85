e59321ff91fbd3c041efdd4d520e424bb401261e
pete85 <info.pete85@gmail.com>
2017-05-08 14:52:43 +0100

    To do list initial commit

---
+++ todo_list/include/head.php
@@ -0,0 +1,23 @@
+<meta charset="utf-8">
+<meta http-equiv="X-UA-Compatible" content="IE=edge">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<meta name="description" content="ESTABLISHED IN 1998, STREAM IS AN INDEPENDENT, PRIVATELY OWNED, FULL SERVICE CREATIVE AGENCY THAT’S PASSIONATE ABOUT BUILDING BRANDS AND SALES. EXCELLENCE IS AT THE HEART OF WHAT WE DO">
+<meta name="author" content="Stream Ltd">
+
+<title>Stream Ltd | Intelligent thinking Inspired ideas </title>
+
+<!-- Bootstrap Core CSS -->
+<link href="css/bootstrap.min.css" rel="stylesheet">
+
+<!-- Hover CSS -->
+<link href="css/hover.css" rel="stylesheet">
+
+<!-- Animate CSS -->
+<link rel="stylesheet" type="text/css" href="css/animate.css">
+
+<!-- Custom Fonts -->
+<link href="font-awesome/css/font-awesome.min.css" rel="stylesheet" type="text/css">
+<link href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,700,300italic,400italic,700italic" rel="stylesheet" type="text/css">
+
+<!-- Custom CSS -->
+<link href="css/styles.css" rel="stylesheet">
---
+++ todo_list/include/scripts.php
@@ -0,0 +1,15 @@
+    <!-- jQuery -->
+    <script src="js/jquery.js"></script>
+
+    <!-- Bootstrap Core JavaScript -->
+    <script src="js/bootstrap.min.js"></script>
+
+    <!-- WOW JS -->
+    <script src="js/wow.min.js"></script>
+
+    <script>
+        new WOW().init();
+    </script>
+
+    <!-- Stream JavaScript -->
+    <script src="js/scripts.js"></script>
