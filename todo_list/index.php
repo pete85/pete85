bd6c800837f53d9ae2f1d51ee781da0dcec7a185
pete85 <info.pete85@gmail.com>
2017-05-09 15:43:35 +0100

    todo list complete

--- todo_list/index.php
+++ todo_list/index.php
@@ -7,37 +7,112 @@
 
 <body>
 
-    <!-- Navigation -->
-    <a id="menu-toggle" href="#" class="btn btn-dark btn-lg toggle"><i class="fa fa-bars"></i></a>
-    <nav id="sidebar-wrapper">
-        <ul class="sidebar-nav">
-            <a id="menu-close" href="#" class="btn btn-light btn-lg pull-right toggle"><i class="fa fa-times"></i></a>
-            <li class="sidebar-brand">
-                <a href="#top" onclick='$("#menu-close").click();'>Start Bootstrap</a>
-            </li>
-            <li>
-                <a href="#top" onclick='$("#menu-close").click();'>Home</a>
-            </li>
-            <li>
-                <a href="#about" onclick='$("#menu-close").click();'>About</a>
-            </li>
-            <li>
-                <a href="#services" onclick='$("#menu-close").click();'>Services</a>
-            </li>
-            <li>
-                <a href="#portfolio" onclick='$("#menu-close").click();'>Portfolio</a>
-            </li>
-            <li>
-                <a href="#contact" onclick='$("#menu-close").click();'>Contact</a>
-            </li>
-        </ul>
-    </nav>
+    <div class="container"> 
+        <div id="todo">
+            <form id="todoForm" method="POST">
+                <fieldset>
+                    <label for="todoTitle">Title</label>
+                    <input name="todoTitle" id="todoTitle" type="text">
+                </fieldset>
+                <fieldset>
+                    <label for="todoContent">Content</label>
+                    <textarea name="todoContent" id="todoContent"></textarea>
+                </fieldset>
+                <fieldset>
+                    <button name="submit" type="submit" id="insert" onclick='clearForm(this.form);'>ADD TASK</button>
+                </fieldset>
+            </form>
+        </div>
+        <div id="todoList">
+        </div>
+    </div>
 
-    <!-- Header -->
-    <header id="top" class="header">
-        
-        
-    </header>
+
+<script>
+    
+    
+    // Fetch the content locally stored in browser's memory
+    // Initiate empty arrays if no content exists
+    function get_titles() {
+        var titles = new Array,
+            titles_str = localStorage.getItem('titles');
+        if (titles_str !== null) {
+            titles = JSON.parse(titles_str); // JSON string to JavaScript
+        }
+        return titles;
+    }
+
+    function get_messages() {
+        var messages = new Array,
+            messages_str = localStorage.getItem('messages');
+        if (messages_str !== null) {
+            messages = JSON.parse(messages_str);
+        }
+        return messages;
+    }
+
+    // Get form input and push it into arrays
+    // get_titles & get_messages retrieve data from browser's memory 
+    function add() {
+        var title = document.getElementById('todoTitle').value,
+            content = document.getElementById('todoContent').value,
+            titles = get_titles(),
+            messages = get_messages();
+
+        titles.push(title);
+        messages.push(content);
+
+        localStorage.setItem('titles', JSON.stringify(titles));
+        localStorage.setItem('messages', JSON.stringify(messages));
+     
+        show();
+     
+        return false;
+    }
+
+    // Retrieve ID of a selected remove link. This ID is an index of a relevant item. Remove item with matching index (splice)  
+    function remove() {
+        var id = this.getAttribute('id'),
+            titles = get_titles(),
+            messages = get_messages();
+
+        titles.splice(id, 1);
+        messages.splice(id, 1);
+        localStorage.setItem('titles', JSON.stringify(titles));
+        localStorage.setItem('messages', JSON.stringify(messages));
+     
+        show();
+     
+        return false;
+    }
+
+    // Display data stored in browser's memory 
+    function show() {
+        var titles = get_titles(),
+            messages = get_messages(),
+            removeTodo = document.getElementsByClassName('remove'),
+            html = '<ul id="tasksList">';
+
+        for(var i=0; i<titles.length; i++) {
+            html += '<li>' + '<h3>' + titles[i] + '</h3>' + '<p>' + messages[i] + '</p>' + '<hr/>' + '<a href="" class="remove" id="' + i  + '">DELETE</a></li>';
+        };
+        html += '</ul>';
+     
+        document.getElementById('todoList').innerHTML = html;
+
+        for (var i=0; i < removeTodo.length; i++) {
+            removeTodo[i].addEventListener('click', remove);
+        };
+
+        return false;
+    }
+
+
+    document.getElementById('insert').addEventListener('click', add);
+    show();
+
+</script>
+
 
 
 
@@ -47,7 +122,7 @@
         <?php include('include/footer.php') ?>
     </footer>
 
-    <?php include('include/scripts.php') ?>
+<?php //include('include/scripts.php') ?>
 
 
 </body>
