ea3174c8156b49125a78f2638758183e0fb63d79
pete85 <info.pete85@gmail.com>
2017-05-19 10:43:10 +0100

    Angular 2 project

---
+++ angular_2/project_1/README.md
@@ -0,0 +1,13 @@
+# [Stream Ltd](http://stream-ltd.com/)
+
+
+
+## Getting Started
+
+
+## Creator
+
+
+## Copyright and License
+
+Copyright 2017 Stream Ltd
---
+++ angular_2/project_1/include/head.php
@@ -0,0 +1,23 @@
+<meta charset="utf-8">
+<meta http-equiv="X-UA-Compatible" content="IE=edge">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<meta name="description" content="">
+<meta name="author" content="Piotr Furmanek">
+
+<title>pete85</title>
+
+<!-- Bootstrap Core CSS -->
+<link href="css/bootstrap.min.css" rel="stylesheet">
+
+<!-- Hover CSS -->
+<link href="css/hover.css" rel="stylesheet">
+
+<!-- Animate CSS -->
+<link rel="stylesheet" type="text/css" href="css/animate.css">
+
+<!-- Custom Fonts -->
+<link href="font-awesome/css/font-awesome.min.css" rel="stylesheet" type="text/css">
+<link href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,700,300italic,400italic,700italic" rel="stylesheet" type="text/css">
+
+<!-- Custom CSS -->
+<link href="css/styles.css" rel="stylesheet">
