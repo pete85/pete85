eca01b5f64899c9376dd9f2a16f8fccd0bac9d81
Piotr Furmanek <pete85@users.noreply.github.com>
2017-06-02 11:21:56 +0100

    Material and Sass

--- angular_2/store-app/src/app/app.module.ts
+++ angular_2/store-app/src/app/app.module.ts
@@ -2,8 +2,11 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
+import { MaterialModule } from '@angular/material';
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import {MdButtonModule, MdCheckboxModule} from '@angular/material';
+
+import 'hammerjs';
 
 import { AppComponent } from './app.component';
 
@@ -15,6 +18,7 @@ import { AppComponent } from './app.component';
     BrowserModule,
     FormsModule,
     HttpModule,
+    MaterialModule,
     BrowserAnimationsModule,
     MdButtonModule, 
     MdCheckboxModule
