124e9f131ecbd6484302d3d4f4b69b07c6ee4b30
Piotr Furmanek <pete85@users.noreply.github.com>
2017-06-26 23:07:24 +0100

    routing

--- angular_2/heroes/src/app/app.module.ts
+++ angular_2/heroes/src/app/app.module.ts
@@ -1,26 +1,37 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
-import { FormsModule }   from '@angular/forms';
-import { MaterialImportModule } from './material-import.module';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-
-import { AppComponent } from './app.component';
-import { HeroDetailComponent } from './hero-detail.component';
-
-import 'hammerjs';
+import { NgModule }                 from '@angular/core';
+import { BrowserModule }            from '@angular/platform-browser';
+import { FormsModule }              from '@angular/forms';
+import { AppComponent }             from './app.component';
+import { HeroDetailComponent }      from './hero-detail.component';
+import { HeroesComponent }          from './heroes.component';
+import { DashboardComponent }       from './dashboard.component';
+import { PageNotFoundComponent }    from './404.component';
+import { HeroService }              from './hero.service';
+import { RouterModule }             from '@angular/router';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    HeroDetailComponent
-  ],
   imports: [
     BrowserModule,
     FormsModule,
-    MaterialImportModule,
-    BrowserAnimationsModule
+    RouterModule.forRoot([
+      {path: '', redirectTo: '/dashboard', pathMatch: 'full'},
+      {path: 'heroes', component: HeroesComponent},
+      {path: 'dashboard', component: DashboardComponent},
+      {path: 'detail/:id', component: HeroDetailComponent},
+      {path: '**', component: PageNotFoundComponent}
+    ])
   ],
-  providers: [],
-  bootstrap: [AppComponent]
+  declarations: [
+    AppComponent,
+    HeroDetailComponent,
+    HeroesComponent,
+    DashboardComponent,
+    PageNotFoundComponent
+  ],
+  providers: [
+    HeroService
+  ],
+  bootstrap: [ AppComponent ]
 })
-export class AppModule { }
+export class AppModule {
+}
--- angular_2/heroes/src/app/hero-detail.component.ts
+++ angular_2/heroes/src/app/hero-detail.component.ts
@@ -1,18 +1,31 @@
-import { Component, OnInit, Input } from '@angular/core';
-import { Hero } from './hero';
+import 'rxjs/add/operator/switchMap';
+import { Component, OnInit }      from '@angular/core';
+import { ActivatedRoute, Params } from '@angular/router';
+import { Location }               from '@angular/common';
 
+import { Hero }         from './hero';
+import { HeroService }  from './hero.service';
 @Component({
   selector: 'hero-detail',
   templateUrl: './hero-detail.component.html',
-  styleUrls: ['./hero-detail.component.css']
+  styleUrls: [ './hero-detail.component.css' ]
 })
 export class HeroDetailComponent implements OnInit {
+  hero: Hero;
 
-  constructor() { }
+  constructor(
+    private heroService: HeroService,
+    private route: ActivatedRoute,
+    private location: Location
+  ) {}
 
-  ngOnInit() {
+  ngOnInit(): void {
+    this.route.params
+      .switchMap((params: Params) => this.heroService.getHero(+params['id']))
+      .subscribe(hero => this.hero = hero);
   }
 
-  @Input() hero: Hero;
-
+  goBack(): void {
+    this.location.back();
+  }
 }
