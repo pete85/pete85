1ad4dfe632f54deee46d4c5631af6640ead57422
Piotr Furmanek <pete85@users.noreply.github.com>
2017-06-27 09:29:28 +0100

    404 page

--- angular_2/routing/src/app/app.module.ts
+++ angular_2/routing/src/app/app.module.ts
@@ -5,6 +5,7 @@ import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 import { AboutComponent } from './about/about.component';
 import { ServicesComponent } from './services/services.component';
+import { PageNotFoundComponent } from './page-not-found.component';
 import { MaterialImportModule } from './material-import.module';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
@@ -15,7 +16,8 @@ import 'hammerjs';
     AppComponent,
     AboutComponent,
     ServicesComponent,
-    HomeComponent
+    HomeComponent,
+    PageNotFoundComponent
   ],
   imports: [
     BrowserModule,
--- angular_2/routing/src/app/app.router.ts
+++ angular_2/routing/src/app/app.router.ts
@@ -6,12 +6,13 @@ import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 import { AboutComponent } from './about/about.component';
 import { ServicesComponent } from './services/services.component';
+import { PageNotFoundComponent } from './page-not-found.component';
 
 export const router: Routes = [
   { path: '', component: HomeComponent },
   { path: 'about', component: AboutComponent },
   { path: 'services', component: ServicesComponent },
-  { path: '**', pathMatch: 'full', component: HomeComponent }
+  { path: '**', pathMatch: 'full', component: PageNotFoundComponent }
 ];
 
 export const routes: ModuleWithProviders = RouterModule.forRoot(router);
