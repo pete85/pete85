18ce1c2a577553e0a042e9734761bf215b79a9a4
Piotr Furmanek <pete85@users.noreply.github.com>
2017-08-03 14:29:03 +0100

    Validation

--- angular_2/forms/src/app/forms/forms.component.ts
+++ angular_2/forms/src/app/forms/forms.component.ts
@@ -1,6 +1,7 @@
 import {Component, OnInit} from '@angular/core';
 import {Employee} from '../models/employee.model';
 import {FormPosterService} from '../services/form-poster.service';
+import {NgForm} from '@angular/forms';
 
 @Component({
   selector: 'app-forms',
@@ -10,11 +11,30 @@ import {FormPosterService} from '../services/form-poster.service';
 export class FormsComponent implements OnInit {
 
   languages = ['English', 'Polish', 'Spanish'];
-  model = new Employee('John', 'Smith', true, 'w1', 'English');
+  model = new Employee('', '', '', true, 'w1', 'default');
+  hasLanguageError = false;
 
   constructor(private formPosterService: FormPosterService) {
   }
 
   ngOnInit() {
   }
+
+  submitForm(form: NgForm) {
+    // validate form
+    this.validateLanguage(this.model.language);
+    if (this.hasLanguageError)
+      return;
+
+    this.formPosterService.postEmployeeForm(this.model);
+  }
+
+  validateLanguage(value) {
+    if (value === 'default')
+      this.hasLanguageError = true;
+    else
+      this.hasLanguageError = false;
+    console.log(this.hasLanguageError);
+  }
+
 }
--- angular_2/forms/src/app/models/employee.model.ts
+++ angular_2/forms/src/app/models/employee.model.ts
@@ -2,6 +2,7 @@ export class Employee {
   constructor(
     public firstName: string,
     public lastName: string,
+    public email: string,
     public isFullTime: boolean,
     public paymentType: string,
     public language: string,
