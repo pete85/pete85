d580b2d34c58ff2bce0df564453afd21e1c5c5b9
Piotr Furmanek <pete85@users.noreply.github.com>
2017-11-26 22:47:36 +0000

    functions finished

--- haymarket/js/main.js
+++ haymarket/js/main.js
@@ -3,43 +3,48 @@
 var articles = [];
 var sliderImage = document.getElementById('imgSlider');
 var artContent = document.getElementById('content');
+var pageNo = document.getElementById('pageNo');
 var artCurrent = document.getElementById('artCurrent');
 var artTotal = document.getElementById('artTotal');
 var prevPage = document.getElementById('previousPage');
 var nextPage = document.getElementById('nextPage');
-var x;
+var x = 0;
 var i = 0;
 
-function createArticle(image, content) {
+function createArticle(pageNo, image, content) {
     return {
+        PageNo: pageNo,
         Image: image,
         Content: content
     };
 }
 
 var art1 = createArticle(
+    'Page 1',
     'img/gallery-slide-1.jpg',
     [
-        {paragraph: 'text1'},
-        {paragraph: 'text2'},
-        {paragraph: 'text3'}
+        {paragraph: 'Nullam accumsan, elit vitae placerat tempor, ex dui fringilla leo, id pretium felis magna ut mauris. Nunc varius diam eget pretium commodo. Nulla ut facilisis ipsum. Ut id maximus velit. Praesent gravida ultricies sagittis. Proin eget nisi metus. Maecenas augue mauris, mollis non ligula non, consequat dignissim nisi. Quisque tellus ante, mattis eget faucibus vel, consectetur eget sem.'},
+        {paragraph: 'Praesent iaculis, elit sed auctor fringilla, velit mauris mollis lacus, at eleifend justo erat sit amet neque. Interdum et malesuada fames ac ante ipsum primis in faucibus. Maecenas posuere consectetur imperdiet. Ut consectetur feugiat justo volutpat lobortis. Sed ut tincidunt diam. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Integer laoreet vel ipsum quis aliquam. In sodales metus felis. Nam eu dui porttitor, tincidunt urna sed, pharetra lorem.'},
+        {paragraph: 'Donec luctus odio commodo tempor finibus. Morbi dictum est non vestibulum congue. Sed a varius ligula, id scelerisque ipsum. Suspendisse lobortis, dui eu consequat viverra, nulla libero blandit tortor, eu auctor ligula sem malesuada tortor. Nam quis posuere justo. Vivamus suscipit risus eget felis condimentum, sit amet elementum est porttitor.'}
     ]
 );
 
 var art2 = createArticle(
+    'Page 2',
     'img/gallery-slide-2.jpg',
     [
-        {paragraph: 'text4'},
-        {paragraph: 'text5'},
-        {paragraph: 'text6'}
+        {paragraph: 'Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. In sodales eros et ultricies condimentum. Nunc lobortis, ipsum at posuere laoreet, turpis elit bibendum augue, eu facilisis urna lacus quis est. Etiam rutrum, diam ut tincidunt facilisis, lorem tortor ultrices ante, a iaculis ex nunc vel eros.'},
+        {paragraph: 'Morbi lorem metus, fringilla vitae molestie hendrerit, semper sit amet dui. In id felis eu metus finibus pellentesque. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; In rutrum est quis sem fringilla, ac tempor est congue. Vestibulum dictum auctor massa. Praesent a facilisis nisi, vel faucibus leo. Sed tortor quam, pharetra porta euismod a, tempus vel tellus.'},
+        {paragraph: 'Aliquam erat volutpat. Sed leo elit, volutpat et neque quis, lobortis facilisis nunc. Vivamus urna erat, imperdiet in vulputate vel, condimentum et ante.'}
     ]
 );
 
 var art3 = createArticle(
+    'Page 3',
     'img/gallery-slide-3.jpg',
     [
-        {paragraph: 'text7'},
-        {paragraph: 'text8'},
+        {paragraph: 'Suspendisse mauris metus, luctus id nunc sit amet, malesuada ultrices diam. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean viverra sollicitudin nisl, vitae tincidunt neque luctus non. Donec tristique congue elit, id dictum ligula eleifend semper. Vivamus sed nibh ac velit euismod placerat facilisis nec magna.'},
+        {paragraph: 'Praesent gravida posuere arcu, eget maximus ante. Duis elit ex, rutrum at metus quis, ultricies dignissim nulla. Nulla at sem eleifend eros convallis commodo et id urna. Donec consectetur luctus ultricies. Ut quis mi in nisl posuere ultricies. Ut et risus eros. Praesent sit amet iaculis massa. Sed tempus volutpat mi vitae pretium.'},
         {paragraph: ''}
     ]
 );
@@ -53,6 +58,8 @@ function getArticles() {
     sliderImage.src = articles[i].Image;
 
     // First article content
+    pageNo.innerHTML = articles[i].PageNo;
+
     for (x in articles[i].Content) {
 
         var paragraph = document.createElement("p");
@@ -72,10 +79,24 @@ function getArticles() {
 // Previous article
 function previous() {
 
-    var i;
+    i = i - 1;
 
-    for (i = 0; i < articles.length; i++) {
-        console.log('test');
+    // First image
+    sliderImage.src = articles[i].Image;
+
+    // article content
+
+    pageNo.innerHTML = articles[i].PageNo;
+
+    while(artContent.firstChild) {
+        artContent.removeChild(artContent.firstChild);
+    }
+
+    for (x in articles[i].Content) {
+
+        var paragraph = document.createElement('p');
+        paragraph.innerHTML = articles[i].Content[x].paragraph;
+        artContent.appendChild(paragraph);
     }
 
     // Current page
@@ -89,21 +110,28 @@ function previous() {
         prevPage.classList.add('disabled');
     }
 
-    console.log('current page: ', parseInt(artCurrent.innerHTML));
 }
 
 // Next article
 function next() {
 
-    // First image
-    sliderImage.src = articles[1].Image;
+    i = i + 1;
 
-    // First article content
-    for (x in articles[1].Content) {
+    // First image
+    sliderImage.src = articles[i].Image;
+
+    // article content
+
+    pageNo.innerHTML = articles[i].PageNo;
+
+    while(artContent.firstChild) {
+        artContent.removeChild(artContent.firstChild);
+    }
+
+    for (x in articles[i].Content) {
 
         var paragraph = document.createElement('p');
-        paragraph.innerHTML = '';
-        paragraph.innerHTML = articles[1].Content[x].paragraph;
+        paragraph.innerHTML = articles[i].Content[x].paragraph;
         artContent.appendChild(paragraph);
     }
 
